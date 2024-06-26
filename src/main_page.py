9145e4720a83515325e3fb39c1ba74c3eff5eafc
gunungpw <gunungpambudiw@gmail.com>
2024-06-26 17:37:41 +0700

    add home_page

---
+++ src/main_page.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from os import path
+
+script_dir = path.dirname(path.abspath(__file__))
+
+st.set_page_config(
+    page_title="Senyummu",
+    page_icon="😊",
+)
+
+with st.columns(3)[1]:
+    st.write("# SENYUMMU 😊")
+    st.image(path.join(script_dir, "assets/logo.png"), width=250)
+    st.write("## Tag Liner")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+    culpa qui officia deserunt mollit anim id est laborum.
+"""
+)
