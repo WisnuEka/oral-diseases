afbe40faa0e6438fbf6bf27994f907ea34b276e4
gunungpw <gunungpambudiw@gmail.com>
2024-06-29 09:22:38 +0700

    add member photo

--- website/pages/04-About_Us.py
+++ website/pages/04-About_Us.py
@@ -5,6 +5,22 @@ dir = path.dirname(path.abspath(__file__))
 image_path = path.join(dir, "../assets/profile2.jpeg")
 
 
+def show_member(member: dict):
+    for person in member:
+        columns = st.columns((0.3, 0.7), vertical_alignment="center")
+        with columns[0]:
+            st.image(path.join(dir, member.get(person)[0]))
+        with columns[1]:
+            st.write(person)
+            for i, info in enumerate(member.get(person)):
+                if i == 0:
+                    pass
+                elif i == 2:
+                    st.page_link(info, label="Linkedin")
+                else:
+                    st.write(info)
+
+
 st.set_page_config(page_title="About Us", page_icon="😊")
 
 st.markdown("<h1 style='text-align: center;'>ABOUT US</h1>", unsafe_allow_html=True)
@@ -13,7 +29,7 @@ st.markdown("### OUR MISSION")
 
 st.markdown(
     """
-    <blockquote style='text-align: center; font-size: 1.75rem; font-family: serif'>
+    <blockquote style='text-align: center; font-size: 1.50rem; font-family: serif'>
     <i>
     Misi kami adalah menyediakan akses yang mudah dan terjangkau bagi seluruh lapisan
     masyarakat untuk memantau dan meningkatkan kesehatan gigi mereka secara mandiri,
@@ -26,80 +42,59 @@ st.markdown(
 
 st.markdown("### OUR TEAM")
 
-st.write("## AI")
-
-data_team_ai = {
+team_ai = {
     "Gunung Pambudi Wibisono": [
-        "../assets/profile.png",
+        "../assets/gunung.jpg",
         "gunungpambudiw@gmail.com",
         "https://www.linkedin.com/in/gunungpw/",
     ],
     "Wisnu Eka Saputra. S": [
-        "../assets/profile.png",
+        "../assets/wisnu.jpg",
         "wisnuekasaputra@hotmail.com",
         "https://id.linkedin.com/in/wisnuekasaputra",
     ],
     "Vauwez Sam El Fareez": [
-        "../assets/profile.png",
+        "../assets/vauwez.jpg",
         "vsefareez@outlook.com",
         "https://www.linkedin.com/in/samfareez/",
     ],
-    "Aisyah Amalia Al Fitri": ["../assets/profile.png", "arsyah291200@gmail.com", "linkedin.com"],
+    "Aisyah Amalia Al Fitri": [
+        "../assets/profile2.jpeg",
+        "arsyah291200@gmail.com",
+        "https://www.linkedin.com/in/aisyahamaliaalfitri/",
+    ],
     "Irvan Achmad Ashari": [
-        "../assets/profile.png",
+        "../assets/profile2.jpeg",
         "irvanachmadashari@gmail.com",
         "https://www.linkedin.com/in/maragopan",
     ],
 }
 
-for person in data_team_ai:
-    columns = st.columns((0.3, 0.7), vertical_alignment="center")
-    with columns[0]:
-        st.image(image_path)
-    with columns[1]:
-        st.write(person)
-        for i, info in enumerate(data_team_ai.get(person)):
-            if i == 0:
-                pass
-            elif i == 2:
-                st.page_link(info, label="Linkedin")
-            else:
-                st.write(info)
+team_data = {
+    "Adi Kurniawan": [
+        "../assets/profile2.jpeg",
+        "adikurniawan917@gmail.com",
+        "https://www.linkedin.com/in/adi-kurniawan-b65a2579/",
+    ],
+    "Gishelawati": [
+        "../assets/ghiselawati.jpg",
+        "gishelawati01@gmail.com",
+        "https://www.linkedin.com/in/gishelawati-129982201/",
+    ],
+    "Nibras Alfaruqiyah": [
+        "../assets/nibras.jpg",
+        "alfaruqiyah.nibras@gmail.com",
+        "https://www.linkedin.com/in/nibras-alfaruqiyah-web-developer-data/",
+    ],
+    "Bagus Akhlaq": [
+        "../assets/profile2.jpeg",
+        "bagusakhlaq@gmail.com",
+        "https://www.linkedin.com/in/bagus-akhlaq",
+    ],
+}
 
+st.write("## Team AI")
+show_member(team_ai)
 
-# with columns[0]:
-#     st.image(image_path)
-#     st.write("Gunung Pambudi Wibisono")
-# with columns[0]:
-#     st.image(image_path)
-#     st.write("Wisnu Eka Saputra S.")
-# with columns[1]:
-#     st.image(image_path)
-#     st.write("Aisyah Amalia Al Fitri")
-# with columns[2]:
-#     st.image(image_path)
-#     st.write("Vauwez Sam El Fareez")
-# with columns[2]:
-#     st.image(image_path)
-#     st.write("Irvan Achmad Ashari")
-
-
-# st.write("#### Team Data")
-
-# columns = st.columns((1, 1, 1))
-
-# with columns[0]:
-#     st.image(image_path)
-#     st.write("Gishelawati")
-# with columns[0]:
-#     st.image(image_path)
-#     st.write("Adi Kurniawan")
-# with columns[1]:
-#     st.image(image_path)
-#     st.write("Bagus Akhlaq")
-# with columns[2]:
-#     st.image(image_path)
-#     st.write("Nibras Alfaruqiyah")
-# with columns[2]:
-#     st.image(image_path)
-#     st.write("Azhani Syahputra")
+st.write("## Team Data")
+show_member(team_data)
