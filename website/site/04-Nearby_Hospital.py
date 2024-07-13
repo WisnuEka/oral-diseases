654ab89206188825332f22dc6a20abeac4a3c66f
gunungpw <gunungpambudiw@gmail.com>
2024-07-13 14:24:24 +0700

    remove title in maps

--- website/site/04-Nearby_Hospital.py
+++ website/site/04-Nearby_Hospital.py
@@ -4,16 +4,16 @@ from streamlit.components.v1 import iframe
 
 st.set_page_config(page_title="Information", page_icon="😊", layout="wide", initial_sidebar_state="collapsed")
 
-# Title
-st.markdown(
-    """
-    <p style='text-align: center; font-size: 2.5rem'>
-    <strong>
-    Nearby Hospital
-    </strong>
-    </p>
-    """,
-    unsafe_allow_html=True,
-)
+# # Title
+# st.markdown(
+#     """
+#     <p style='text-align: center; font-size: 2.5rem'>
+#     <strong>
+#     Nearby Hospital
+#     </strong>
+#     </p>
+#     """,
+#     unsafe_allow_html=True,
+# )
 
-iframe("https://gunungpw.github.io/nearby-map/", height=550)
+iframe("https://gunungpw.github.io/nearby-map/", height=600)
